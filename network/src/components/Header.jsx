8f47722a5979237c377eeab595376555bc6ec73d
Alexey <belshina25@gmail.com>
2020-12-09 13:58:00 +0300

    feat: start create components, create header, nav and profile components

---
+++ network/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+  return <header className="header">
+    <img src="https://clipart-best.com/img/intel/intel-clip-art-10.png" alt="logo" />
+  </header>
+}
+
+export default Header;
